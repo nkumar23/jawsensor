cf956fb7f54622f401021668dd2d06634183a47c
Nikhil <nkumar.pro@gmail.com>
2020-02-28 11:01:45 -0500

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,11 @@
-const char WIFI_SSID[] = "sandbox370";
-const char WIFI_PASSWORD[] = "+s0a+s03!2gether?";
 
-//const char WIFI_SSID[] = "Essence of Essex";
-//const char WIFI_PASSWORD[] = "tinywind488";
 
-const char MQTT_BROKER[] = "itpdtd.com";
+//const char WIFI_SSID[] = "wifi network name"
+//const char WIFI_PASSWORD[] = "wifi network password";
+
+const char MQTT_BROKER[] = "xxxxxx.com"
 const int MQTT_PORT = 8883;
-const char MQTT_USER[] = "nikhil";
-const char MQTT_PASSWORD[] = "voucher-paternal-soever";
+const char MQTT_USER[] = "username";
+const char MQTT_PASSWORD[] = "password";
 
-const String DEVICE_ID = "jaw";
+const String DEVICE_ID = "yyyyyy";
